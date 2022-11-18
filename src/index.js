ee71b23b664bc637a5c99384c9e84ca1a6f7f128
lenovo <Athif binu>
2022-11-18 22:42:40 +0530

    learn err handling

--- src/index.js
+++ src/index.js
@@ -11,27 +11,32 @@ class App extends React.Component {
 
     // THIS IS THE ONLY TIME we do direct assignment
     // to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         // we called setstate!!!!
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-
-
 
 
 
